b38b4fc8ab19ba8a7458777bbbe8c4295e7e5764
rishika singh <singhrishikasingh0599@gmail.com>
2025-06-17 13:14:15 +0530

    update

---
+++ src/components/Weather/temp.js
@@ -0,0 +1,68 @@
+import React,{useEffect, useState} from 'react';
+import Weathercard from "./weathercard";
+import './style.css';
+import { cleanup } from '@testing-library/react';
+
+const Temp = () => {
+    const[searchValue,setSearchValue]=useState("pune");
+    const[tempInfo,setTempInfo]=useState({});
+    const getWeatherInfo=async()=>{
+        try {
+            let url=`https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=f57716b78badaebb9f8219e6663d0964`
+            const res= await fetch(url)
+            const data= await res.json();
+
+            const{ temp,humidity,pressure }=data.main;
+            const{ main:weathermood }=data.weather[0];
+            const { name }=data;
+            const {speed}=data.wind;
+            const{country,sunset}=data.sys;
+            
+            
+            const myNewWeatherInfo={
+                temp,
+                humidity,
+                pressure,
+                weathermood,
+                name,
+                country,
+                sunset,
+               
+              };
+              setTempInfo(myNewWeatherInfo);
+                } catch (error) {
+            console.log(error);
+            
+            
+        };
+    };
+   useEffect(()=> {
+    getWeatherInfo();
+   },[]);
+    
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="Search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e)=>setSearchValue(e.target.value)}
+          />
+          <button className="searchButton" 
+          type="button"
+          onClick={getWeatherInfo}>
+            Search
+          </button>
+        </div>
+      </div>
+<Weathercard tempInfo={tempInfo}/>
+          </>
+  );
+};
+
+export default Temp;
